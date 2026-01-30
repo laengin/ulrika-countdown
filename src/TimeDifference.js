5332cfd426106a927c143af1af9e4ee4a16b27c3
laengin <76446604+laengin@users.noreply.github.com>
2026-01-30 17:29:58 +0100

    Bugfix Time Difference

--- src/TimeDifference.js
+++ src/TimeDifference.js
@@ -1,16 +1,13 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 
-const TimeDifference = (props) => {
-  const [timeDifference, setTimeDifference] = useState(
-    getTimeDifference(props.targetDateTime)
-  );
+const TimeDifference = props => {
+  const [timeDifference, setTimeDifference] = useState(getTimeDifference(props.targetDateTime));
 
   useEffect(() => {
     const timerId = setInterval(() => {
       setTimeDifference(getTimeDifference(props.targetDateTime));
     }, 1000);
-
     return () => clearInterval(timerId);
   }, [props.targetDateTime]);
 
@@ -23,7 +20,7 @@ const TimeDifference = (props) => {
     const now = new Date();
     const targetTime = new Date(target);
     const diffInMs = Math.max(targetTime - now, 0); //Verhindert negative Werte
-
+    /*
     const months = calculateMonthDifference(now, targetTime);
     const seconds = Math.floor((diffInMs / 1000) % 60);
     const minutes = Math.floor((diffInMs / (1000 * 60)) % 60);
@@ -31,16 +28,80 @@ const TimeDifference = (props) => {
     hours = hours + (now.getTimezoneOffset() / -60);
     const days = Math.floor(diffInMs / (1000 * 60 * 60 * 24));
     const weeks = Math.floor(days / 7);
-    return { days, weeks, months, hours, minutes, seconds };
+*/
+    const months = calculateDateDifference(now, targetTime).months;
+    const years = calculateDateDifference(now, targetTime).years;
+    const seconds = calculateDateDifference(now, targetTime).seconds;
+    const minutes = calculateDateDifference(now, targetTime).minutes;
+    const hours = calculateDateDifference(now, targetTime).hours;
+    const days = calculateDateDifference(now, targetTime).days;
+    // const weeks = Math.floor(days / 7);
+    const weeks = calculateDateDifference(now, targetTime).weeks;
+
+    return { days, weeks, months, years, hours, minutes, seconds };
   }
 
+  function calculateDateDifference(startDate, endDate) {
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+
+    // Zeitdifferenz in Millisekunden
+    // const diffInMs = end - start;
+
+    // Monate berechnen
+    let months = (end.getFullYear() - start.getFullYear()) * 12;
+    months += end.getMonth() - start.getMonth();
+    if (end.getDate() <= start.getDate()) {
+      months--; // Korrigieren, wenn der Tag im Enddatum kleiner ist
+    }
+
+    // Jahre berechnen
+    const years = Math.floor(months / 12);
+    months = months % 12;
+
+    // Now Adjust startdate to calculate the weeks and days
+    let newStart = new Date(start);
+    if (start.getMonth() + months > 11) {
+      newStart.setFullYear(start.getFullYear() + 1, start.getMonth() + months - 12);
+    } else {
+      newStart.setFullYear(start.getFullYear(), start.getMonth() + months);
+    }
+    /*
+    newStart.setHours(0);
+    newStart.setMinutes(0);
+    newStart.setSeconds(0);
+    newStart.setMilliseconds(0);
+        */
+    // console.log(newStart);
+
+    const adjDiffInMs = Math.max(end - newStart, 0); //Verhindert negative Werte
+
+    //  Rest Tage berechnen
+    // const days = diffInMs / (1000 * 60 * 60 * 24);
+    const days = Math.floor(adjDiffInMs / (1000 * 60 * 60 * 24));
+    // console.log(days);
+    // restliche Wochen berechnen
+    const weeks = Math.floor(days / 7);
+    // console.log(weeks);
+    // restliche Zeit berechnen
+    const seconds = Math.floor((adjDiffInMs / 1000) % 60);
+    const minutes = Math.floor((adjDiffInMs / (1000 * 60)) % 60);
+    const hours = Math.floor((adjDiffInMs / (1000 * 60 * 60)) % 24);
+    // console.log(newStart.getTimezoneOffset());
+    // hours = Math.floor(hours + (start.getTimezoneOffset() / -60));
+
+    return {
+      days: days,
+      weeks: weeks,
+      months: months,
+      years: years,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds
+    };
+  }
   function calculateWeekDifference(startDate, endDate) {
-    if (
-      !(startDate instanceof Date) ||
-      !(endDate instanceof Date) ||
-      isNaN(startDate) ||
-      isNaN(endDate)
-    ) {
+    if (!(startDate instanceof Date) || !(endDate instanceof Date) || isNaN(startDate) || isNaN(endDate)) {
       console.error("Ungültige Datumsangaben:", startDate, endDate);
       return 0;
     }
@@ -53,12 +114,7 @@ const TimeDifference = (props) => {
   }
 
   function calculateMonthDifference(startDate, endDate) {
-    if (
-      !(startDate instanceof Date) ||
-      !(endDate instanceof Date) ||
-      isNaN(startDate) ||
-      isNaN(endDate)
-    ) {
+    if (!(startDate instanceof Date) || !(endDate instanceof Date) || isNaN(startDate) || isNaN(endDate)) {
       console.error("Ungültige Datumsangaben:", startDate, endDate);
       return 0;
     }
@@ -70,37 +126,36 @@ const TimeDifference = (props) => {
     return years * 12 + months + daysAdjustment;
   }
 
-  if (
-    timeDifference.months === 0 &&
-    timeDifference.days === 0 &&
-    timeDifference.hours === 0
-  ) {
+  if (timeDifference.months === 0 && timeDifference.days === 0 && timeDifference.hours === 0) {
     return <div style={{ color: "red" }}>Der Zielzeitpunkt ist erreicht!</div>;
   }
-
+  const strMonat = timeDifference.months === 1 ? "Monat" : "Monate";
+  const strWeek = timeDifference.weeks === 1 ? "Woche" : "Wochen";
+  const strYear = timeDifference.years === 1 ? "Jahr" : "Jahre";
+  const strDay = timeDifference.days % 7 === 1 ? "Tag" : "Tage";
   return (
     <div className="TimeDifference container">
       <div>
         <p>{props.startText}</p>
       </div>
       <div className="row">
-          <p>
-            {timeDifference.weeks > 0 && `${timeDifference.weeks} Wochen `}
-            {timeDifference.days > 0 && `${timeDifference.days % 7} Tage und `}
-            {timeDifference.hours.toString().padStart(2, "0")}:
-            {timeDifference.minutes.toString().padStart(2, "0")}:
-            {timeDifference.seconds.toString().padStart(2, "0")}
-          </p>
+        <p className="ClockFontSize">
+          {timeDifference.years > 0 && `${timeDifference.years} ${strYear} `}
+          {timeDifference.months > 0 && `${timeDifference.months} ${strMonat} `}
+          {timeDifference.weeks > 0 && `${timeDifference.weeks} ${strWeek} `}
+          {timeDifference.days > 0 && `${timeDifference.days % 7} ${strDay} und  `}
+          {timeDifference.hours.toString().padStart(2, "0")} h : {timeDifference.minutes.toString().padStart(2, "0")} m : {timeDifference.seconds.toString().padStart(2, "0")} s
+        </p>
       </div>
       <div>
-      <p>{props.endText}</p>
-    </div>
+        <p>{props.endText}</p>
+      </div>
     </div>
   );
 };
 
 TimeDifference.propTypes = {
-  targetDateTime: PropTypes.string.isRequired, // Muss ein gültiger Datum-String sein
+  targetDateTime: PropTypes.string.isRequired // Muss ein gültiger Datum-String sein
 };
 
 export default TimeDifference;
