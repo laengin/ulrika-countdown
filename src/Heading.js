ff155554efc8244255fc48b671ff015652f40a45
laengin <76446604+laengin@users.noreply.github.com>
2026-01-30 17:36:04 +0100

    Change Heading

--- src/Heading.js
+++ src/Heading.js
@@ -1,9 +1,10 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function Heading() {
   return (
     <div className="Heading container">
-      <h1>Ulrika's Countdown</h1>
+      <h1>Ulrika's Countdown: 30. Juni. 2026</h1>
     </div>
   );
 }
--- src/TimeDifference.js
+++ src/TimeDifference.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import PropTypes from "prop-types";
 
 const TimeDifference = props => {
   const [timeDifference, setTimeDifference] = useState(getTimeDifference(props.targetDateTime));
